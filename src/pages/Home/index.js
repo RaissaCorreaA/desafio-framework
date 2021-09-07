d6126db65cce3cbaefbf05893695cc285cbb0c5b
Raíssa Corrêa <raissacorreaa@gmail.com>
2021-09-07 16:38:38 -0300

    add home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+let Home = () => {
+  return (
+    <div id="homePage">
+      <div className="homeAbout"></div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home/index";
+import Posts from "./pages/Posts/index";
+import Albuns from "./pages/Albuns/index";
+import ToDos from "./pages/ToDos/index";
+
+let router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/posts" component={Posts} />
+      <Route path="/albuns" component={Albuns} />
+      <Route path="/to-dos" component={ToDos} />
+    </Switch>
+  );
+};
+export default router;
